67d99bcff96d3b7214ec8daeb747862704b5ed53
Luis Enrique Arias Hernandez <larias@arkusnexus.com>
2022-11-11 18:17:38 -0800

    Create clean architecture main layer projects (presentation, application, infrastructure and domain)

---
+++ CleanArchitecture.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchitecture.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ CleanArchitecture.Incfrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchitecture.Incfrastructure
+{
+    public class Class1
+    {
+    }
+}
